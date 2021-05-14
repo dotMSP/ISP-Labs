387f37ebb30ada0a3f908647c7d48060cfc53112
dotMSP <79195263+dotMSP@users.noreply.github.com>
2021-05-15 01:39:20 +0300

    Add files via upload

---
+++ SimpleMath5/SimpleMath5/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleMath5
+{
+    public class Class1
+    {
+        public double Addition(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtraction(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Division(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
